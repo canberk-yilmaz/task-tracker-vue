d9ee65cde75975527b10b06a01e153d05d1b64f8
canberk-yilmaz <cyilmaz089@gmail.com>
2021-10-05 00:20:34 +0300

    Task Tracker App - Vue

---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      v-show="homePage"
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+
+export default {
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+    },
+    showAddTask: {
+      type: Boolean,
+    },
+  },
+  components: {
+    Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+  methods: {
+    onClick() {
+      this.$emit("toggle-add-task");
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
