a4d5d2a91a7e05c5c50a74415dfac4af5efa90c2
canberk-yilmaz <cyilmaz089@gmail.com>
2021-10-05 00:30:46 +0300

    Config updated

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,8 @@ module.exports = {
                 pathRewrite: { '^/api': '/' },
             },
         },
+
+        publicPath: process.env.NODE_ENV === 'production'
+            ? '/task-tracker-vue/'
+            : '/'
     }
-}
